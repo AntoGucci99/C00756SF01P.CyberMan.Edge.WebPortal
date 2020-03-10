caf8c5ea48ea40adbe3054cd70953b1611a7048e
giovanni.antonucci <giovanni.antonucci@beantech.it>
2020-03-10 16:08:20 +0100

    Aggiunta web api

---
+++ C00756SF01P.CyberMan.Edge.WebAPI/C00756SF01P.CyberMan.Edge.WebAPI/AppContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C00756SF01P.CyberMan.Edge.WebAPI
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options)
+          : base(options)
+        {
+        }
+
+        public DbSet<Status> Statuses{ get; set; }
+    }
+}
---
+++ C00756SF01P.CyberMan.Edge.WebAPI/C00756SF01P.CyberMan.Edge.WebAPI/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C00756SF01P.CyberMan.Edge.WebAPI
+{
+    public class Status
+    {
+        public int Id { get; set; }
+        public string NameStatus { get; set; }
+        public string StatusCurrent { get; set; }
+
+    }
+}
